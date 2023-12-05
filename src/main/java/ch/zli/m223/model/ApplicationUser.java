f317a382cf67838461c6bce5d78859032aa028b6
bomoba <142784300+bomoba@users.noreply.github.com>
2023-12-05 08:35:59 +0000

    ApplicationUser

--- src/main/java/ch/zli/m223/model/ApplicationUser.java
+++ src/main/java/ch/zli/m223/model/ApplicationUser.java
@@ -1,63 +1,57 @@
 package ch.zli.m223.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-
+import javax.persistence.*;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
-@Member
+@Entity
 @NamedQueries({
     @NamedQuery(name = "ApplicationUser.findByEmail", query = "SELECT m FROM ApplicationUser m WHERE m.email = :email")
 })
 public class ApplicationUser {
 
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Schema(readOnly = true)
-  private Long memberID;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(readOnly = true)
+    private Long memberID;
 
-  @Column(nullable = false, unique = true)
-  private String email;
+    @Column(nullable = false, unique = true)
+    private String email;
 
-  @Column(nullable = false)
-  private String password;
+    @Column(nullable = false)
+    private String password;
 
-  @Column
-  private String firstName;
+    @Column
+    private String firstName;
+    
+    public Long getMemberID() {
+        return memberID;
+    }
 
-  public Long getId() {
-    return memberID;
-  }
+    public void setMemberID(Long memberID) {
+        this.memberID = memberID;
+    }
 
-  public void setId(Long memberID) {
-    this.memberID = memberID;
-  }
+    public String getEmail() {
+        return email;
+    }
 
-  public String getEmail() {
-    return email;
-  }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+    public String getPassword() {
+        return password;
+    }
 
-  public String getPassword() {
-    return password;
-  }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-  public void setPassword(String password) {
-    this.password = password;
-  }
+    public String getFirstName() {
+        return firstName;
+    }
 
-  public String getFirstName() {
-    return firstName;
-  }
-
-  public void setFirstname(String firstName) {
-    this.firstName = firstName;
-  }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 }
